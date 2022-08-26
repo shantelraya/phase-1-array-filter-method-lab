6e7e7cc14bc03d4115392f76285f95f3075b3ebb
parschax@gmal.com <shantel@DESKTOP-D024ROM.localdomain>
2022-08-26 10:16:27 +0300

    done assignments

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array, string) {
+    const names = array.filter(name => name.toLowerCase() === string.toLowerCase());
+
+    return names;
+}
+function fuzzyMatch(driver, testName) {
+    return driver.filter(
+        (match) => 
+        match.toLowerCase().indexOf(testName.toLowerCase()) === 0
+    );
+}
+function matchName(driver, string) {
+    const match = driver.filter(details => details.name === string);
+
+    return match;
+}
+
+console.log(matchName(drivers, "Shantel"));
